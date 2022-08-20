18300fe86a19c31089c692eb4d9a5c5eb2419a3e
wjimh314 <wjimh314@gmail.com>
2022-08-20 14:55:41 -0400

    made .gitignore file, added more to the seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department(id, department_name)
+VALUES ("1",sales),
+       ("2",Engineering),
+       ("3",fiance),
+       ("4",legal),
+
+INSERT roles(id,title,department,salary,table)
+VALUES ("1", sales lead,sales,100000),
+       ("2",sales person,sales,80000),
+       ("3",lead Engineer,Engineer,120000),
+       ("4",software Engineer,Engineering,130000),
+       ("5",accountant manager,fiance,1000000),
+       ("6",accountant,fiance,100000),
+       ("7",lawyer,legal,120000),
+       ("8",legal team leader,150000),
