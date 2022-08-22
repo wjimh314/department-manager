7ce33ad860ca1b4abd6f056e79d9ee7281984464
wjimh314 <wjimh314@gmail.com>
2022-08-22 06:39:44 -0400

    started server.js

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,25 @@
 
 DROP DATABASE IF EXISTS departments_db;
 CREATE DATABASE departments_db
-
-USE departments_db;
+USE department_db;
 
 CREATE TABLE departments(
     id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(100)
 );
+
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    department_id INT,
+    salary DECIMAL,
+
+);
+
+CREATE TABLE employees(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30)
+    last_name VARCHAR(30)
+    role_id INT
+    manager_id INT
+    
