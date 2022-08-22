940e301a50134e59752cbf272f962fa694ef5712
wjimh314 <wjimh314@gmail.com>
2022-08-22 18:09:16 -0400

    index edits

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const inquirer = require("inquirer");
-const questions = [
+const mysql = require("mysql2");
+const PORT = process.env.PORT || 3001;
+
+inquirer.prompt([
 	{
-		type: "input",
-		name: "managerName",
-		message: "what is the managers name",
+		type: "list",
+		name: "options",
+		message: "what would you like to do?",
+		choices: [
+			"view all departments",
+			"view all roles",
+			"view all employees",
+			"add a department",
+			" add a role",
+			"add an employee",
+			"update an employee role",
+		],
 	},
-];
+]);
 
 function startPrompts() {
 	inquirer.prompt(questions);
