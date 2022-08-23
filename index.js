3591096aabc916a5f12a80acf08aa19458e3c700
wjimh314 <wjimh314@gmail.com>
2022-08-23 13:16:01 -0400

    added switch statement

--- index.js
+++ index.js
@@ -13,27 +13,92 @@ let connection = mysql.createConnection(
 	},
 	console.log(`Connected to the et_db; database.`)
 );
+console.table("The Employee Tracker");
 
-const questions = [
-	{
-		type: "list",
-		name: "options",
-		message: "what would you like to do?",
-		choices: [
-			"view all departments",
-			"view all roles",
-			"view all employees",
-			"add a department",
-			" add a role",
-			"add an employee",
-			"update an employee role",
-			"exit",
-		],
-	},
-];
+startPrompts = () => {
+	inquirer
+		.prompt([
+			{
+				type: "list",
+				name: "options",
+				message: "what would you like to do?",
+				choices: [
+					"view all departments",
+					"view all roles",
+					"view all employees",
+					"add a department",
+					" add a role",
+					"add an employee",
+					"update an employee role",
+					"exit",
+				],
+			},
+		])
+		.then((response) => {
+			switch (options.response) {
+				case "View all departments":
+					viewAllDepartments();
+					break;
+				case "View all roles":
+					viewAllRoles();
+					break;
+				case "View all employees":
+					viewAllEmployees();
+					break;
+				case "View all employees by manager":
+					viewAllEmployeesByManager();
+					break;
+				case "Add a department":
+					addADepartment();
+					break;
+				case "Add a role":
+					addARole();
+					break;
+				case "Add an employee":
+					addAnEmployee();
+					break;
+				case "Update employee's role":
+					updateEmployeeRole();
+					break;
+				case "Update employee's manager":
+					updateEmployeesManager();
+					break;
+				case "Remove a department":
+					removeADepartment();
+					break;
+				case "Remove a role":
+					removeARole();
+					break;
+				case "Remove an employee":
+					removeAnEmployee();
+					break;
+				case "View total salary of department":
+					viewDepartmentSalary();
+					break;
+				case "Exit program":
+					connection.end();
+					console.log(
+						"\n You have exited the employee management program. Thanks for using! \n"
+					);
+
+					break;
+			}
+		});
+};
+
+console.table(viewAllDepartments);
 
 function startPrompts() {
-	inquirer.prompt(questions);
+	inquirer.prompt(questions).then((answers) => {
+		function startPrompts() {
+			inquirer.prompt(questions).then((answers) => {
+				connection
+					.promise()
+					.query("SELECT department.id, department.name FROM department");
+			});
+		}
+		console.table(answers);
+	});
 }
 
 startPrompts();
